1354cfd5442fefd3ebdf930ce5787a26313ad648
Sameh <sameh.elwakeel75@gmail.com>
2024-06-28 23:47:06 +0300

    Added Chakara UI and custom font

---
+++ src/assets/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+import '@fontsource/poppins'
+
+const theme = extendTheme({
+    fonts: {
+        text: 'Poppins, sans-serif',
+    }
+});
+
+export default theme;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from './assets/theme.ts'
+
+import '@fontsource/poppins'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ChakraProvider theme={theme}>
     <App />
+  </ChakraProvider>
 )
